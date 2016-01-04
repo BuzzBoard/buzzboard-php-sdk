1f6a0aee0db347bba4d00fc1a77409d27613464e
Mir Adnan <contact@miradnan.com>
2016-01-04 15:14:28 +0530

    Updated test client API Key

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,8 @@ date_default_timezone_set('America/New_York');
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-define('TEST_API_KEY', md5(152));
-define('TEST_PROFILE_ID', md5(1307463));
+define('TEST_API_KEY', '');
+define('TEST_PROFILE_ID', '');
 
 echo PHP_EOL;
 print "API_KEY = " . TEST_API_KEY . PHP_EOL;
